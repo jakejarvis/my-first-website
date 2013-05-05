1fe4d4560361d11868ac4108d34e9eddd6180821
Jake Jarvis <jakejarvis@gmail.com>
2013-05-05 17:56:42 -0400

    first commit

---
+++ games/pacman/pacman.php
@@ -0,0 +1,44 @@
+
+<? 
+
+/*
+include("../../global.php");
+
+$headtag = '';
+
+//  below is the bodytag,headtag,tablecolor	
+		
+head("",$headtag,"",$cat);
+
+*/
+?>	
+		
+<!-- START MAIN BODY CONTENT-->	
+
+
+<!--TITLE HERE-->
+
+<DIV ALIGN="center"><FONT SIZE="4"><B>Pacman</B></FONT>
+
+<BR><BR><BR>
+<!--DESCRIPTION HERE-->
+
+The old arcade classic video game reborn in a java applet!  Great fun!
+<BR><BR>
+<!--BODY CODE HERE-->
+<applet code="PacManApp.class" width=526 height=427></applet><BR><BR>
+
+Author: Jean Francois Buisson   
+
+<BR><BR>
+
+<A href="http://www.javafile.com/games/pacman/pacman.zip"><B>Download the Script</B></A></P>
+
+</DIV>	
+		
+		
+<!-- END MAIN BODY CONTENT-->	
+		
+
+
+<? footer(); ?>	
